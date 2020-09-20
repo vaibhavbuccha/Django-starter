05b2ef3a497eb2859d89cbbb8cda1cf9b2564c02
vaibhavbuccha <jainvaibhav415@gmail.com>
2020-09-20 12:20:18 +0530

    Register app in admin

--- telusko/settings.py
+++ telusko/settings.py
@@ -127,3 +127,10 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+
+# add media path
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
--- travello/views.py
+++ travello/views.py
@@ -2,26 +2,5 @@ from django.shortcuts import render
 from .models import Destination
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.id = 1
-    dest1.name = 'Mumbai'
-    dest1.desc = '''A city that never sleep's.'''
-    dest1.price = 700
-    dest1.image = 'destination_1.jpg'
-    dest1.offer = True
-    dest2 = Destination()
-    dest2.id = 1
-    dest2.name = 'Rajasthan'
-    dest2.desc = '''A city Of traditions.'''
-    dest2.price = 500
-    dest2.image = 'destination_2.jpg'
-    dest2.offer = False
-    dest3 = Destination()
-    dest3.id = 1
-    dest3.name = 'kolkata'
-    dest3.desc = '''A city Of joy.'''
-    dest3.price = 650
-    dest3.image = 'destination_3.jpg'
-    dest3.offer = True
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
